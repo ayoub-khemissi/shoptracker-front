189adc6ff3821b1f3062659f1e726d61a7b53937
Sungis18 <khemayoub@gmail.com>
2024-07-18 22:00:55 +0200

    feat: prepare global layout

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${montserrat.className} bg-contrast-dark text-primary-dark`}>{children}</body>
+      <body className={`${montserrat.className} bg-contrast-dark text-primary-dark lg:px-40 lg:py-10 md:px-20 md:py-6 px-10 py-2`}>
+        <header className=""></header>
+        <main className="">
+          {children}
+        </main>
+        <footer className=""></footer>
+      </body>
     </html>
   );
 }

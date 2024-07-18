d82d8bc700f4afff2cb4d3cac1d6b0968fecaa29
Sungis18 <khemayoub@gmail.com>
2024-07-18 19:09:17 +0200

    feat: Switch css transition

--- app/components/Switch.js
+++ app/components/Switch.js
@@ -1,7 +1,7 @@
-const Switch = ({ className = "", checked, onChange }) => {
+const Switch = ({ className, checked, onChange }) => {
   return (
-    <div className={`${className} border border-primary rounded-full w-12 h-6 px-1 flex items-center ${checked ? "justify-end" : "justify-start"}`} onClick={onChange} >
-      <div className={`rounded-full bg-primary w-4 h-4`}></div>
+    <div className={`${className} border border-primary rounded-full p-1 w-12 flex justify-center items-center transition-transform duration-300`} onClick={onChange} >
+      <div className={`rounded-full bg-primary p-2 transform transition-transform duration-300 ${checked ? 'translate-x-3/4' : '-translate-x-3/4'}`} ></div>
     </div>
   );
 }

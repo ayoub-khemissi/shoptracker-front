fc6073db03531087c33395c65e6aff7b0a7cbcd4
Sungis18 <khemayoub@gmail.com>
2024-07-18 21:19:02 +0200

    fix: update Switch size

--- app/components/Switch.js
+++ app/components/Switch.js
@@ -1,7 +1,7 @@
 const Switch = ({ className, checked, onChange }) => {
   return (
-    <div className={`${className} border border-primary rounded-full p-1 w-12 flex justify-center items-center transition-transform duration-300`} onClick={onChange} >
-      <div className={`rounded-full bg-primary p-2 transform transition-transform duration-300 ${checked ? 'translate-x-3/4' : '-translate-x-3/4'}`} ></div>
+    <div className={`${className} border border-primary rounded-full p-0.5 w-14 flex justify-center items-center transition-transform duration-300`} onClick={onChange} >
+      <div className={`rounded-full bg-primary p-2.5 transform transition-transform duration-300 ${checked ? 'translate-x-3/4' : '-translate-x-3/4'}`} ></div>
     </div>
   );
 }

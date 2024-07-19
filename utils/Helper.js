b043dbe04c29c8cf94f33c19ca98a97aa09e7a3e
Sungis18 <khemayoub@gmail.com>
2024-07-19 22:44:31 +0200

    feat: Helper functions - convertMilliseconds: convert ms into text - formatMonthlyAnnuallyPrice: format price regarding the chosen billing plan

---
+++ utils/Helper.js
@@ -0,0 +1,26 @@
+export function convertMilliseconds(ms) {
+  const second = 1000;
+  const minute = 60000;
+  const hour = 3600000;
+  const day = 24 * hour;
+
+  if (ms < second) {
+    return "less than a second";
+  } else if (ms < minute) {
+    let seconds = Math.floor(ms / second);
+    return seconds + (seconds === 1 ? " second" : " seconds");
+  } else if (ms < hour) {
+    let minutes = Math.floor(ms / minute);
+    return minutes + (minutes === 1 ? " minute" : " minutes");
+  } else {
+    let hours = Math.floor((ms % day) / hour);
+    return hours + (hours === 1 ? " hour" : " hours");
+  }
+}
+
+export function formatMonthlyAnnuallyPrice(monthlyAnnually, price) {
+  if (!price || price == 0) { return "0"; }
+  const annuallyCalc = Math.round(price * 12 * 0.75) - 0.01;
+
+  return String(monthlyAnnually ? annuallyCalc : price).replace(".", ",");
+}

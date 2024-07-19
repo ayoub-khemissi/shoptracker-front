28686d025358a55adef358d09d0b8ce2ff8de522
Sungis18 <khemayoub@gmail.com>
2024-07-19 22:46:56 +0200

    feat: update Title component - add default font-size to 4xl

--- app/components/Title.js
+++ app/components/Title.js
@@ -3,9 +3,8 @@ import { Lexend } from "next/font/google";
 const lexend = Lexend({ weight: "700", subsets: ["latin"] });
 
 const Title = ({ children, className }) => {
-
   return (
-    <h2 className={`${lexend.className} uppercase ${className}`}>{children}</h2>
+    <h2 className={`${lexend.className} uppercase text-4xl ${className}`}>{children}</h2>
   );
 }
 

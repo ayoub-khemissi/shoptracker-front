5aff2a300aae44c9440147d6a9abaca8c9a3c323
Sungis18 <khemayoub@gmail.com>
2024-07-21 02:41:33 +0200

    fix: input display adjustments

--- app/components/Input.js
+++ app/components/Input.js
@@ -4,9 +4,9 @@ const montserrat = Montserrat({ weight: "500", subsets: ["latin"] });
 
 const Input = ({ className = "", labelText = null, type, placeholder, value, onChange }) => {
   return (
-    <div className={`flex flex-col ${className}`}>
+    <div className={`flex flex-col w-full ${className}`}>
       {labelText &&
-        <label className={`${montserrat.className} uppercase py-1`}>{labelText}</label>
+        <label className={`${montserrat.className} uppercase pb-1`}>{labelText}</label>
       }
       <input className="py-2 px-4 border-2 rounded border-primary bg-contrast placeholder-gray" placeholder={placeholder} value={value} type={type} onChange={onChange} />
     </div>

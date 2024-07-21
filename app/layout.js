0e74fbd2b907ac1cacfabbb6a243a28d888a18fd
Sungis18 <khemayoub@gmail.com>
2024-07-21 02:37:22 +0200

    feat: make footer always be at bottom of the page + text typo

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,7 @@ const montserrat = Montserrat({ weight: "500", subsets: ["latin"] });
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
-      <body className={`${montserrat.className} bg-contrast text-primary`}>
+      <body className={`${montserrat.className} bg-contrast text-primary flex flex-col h-screen`}>
         <header className="flex flex-wrap sm:justify-between justify-center items-center lg:px-40 lg:py-10 px-20 py-6">
           <div>
             <Link href="/">
@@ -18,13 +18,13 @@ export default function RootLayout({ children }) {
             </Link>
           </div>
           <nav className="flex sm:justify-end justify-center items-center space-x-4">
-            <NavLink href="/register">Signup</NavLink>
-            <NavLink href="/login">Signin</NavLink>
+            <NavLink href="/register">Sign Up</NavLink>
+            <NavLink href="/login">Sign In</NavLink>
           </nav>
         </header>
-        <>
+        <div className="flex-1">
           {children}
-        </>
+        </div>
         <footer className="bg-contrast-alt flex flex-wrap justify-center items-center space-x-4 lg:px-40 lg:py-10 px-20 py-6">
           <p className="text-sm">© 2024 <ShopTrackerLogo className="text-sm" /></p>
           <FooterLink href="terms-of-use">Terms of Use</FooterLink>

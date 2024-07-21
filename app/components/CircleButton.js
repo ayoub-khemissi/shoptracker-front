9535dfe991a1f4c1d00ecdfba7865de042187235
Sungis18 <khemayoub@gmail.com>
2024-07-21 02:41:15 +0200

    fix: CircleButton always being a circle and children centered

--- app/components/CircleButton.js
+++ app/components/CircleButton.js
@@ -1,6 +1,6 @@
 const CircleButton = ({ children, className = "", onClick }) => {
   return (
-    <button className={`bg-transparent rounded-full border-primary border p-3 m-0 ${className}`} onClick={onClick}>
+    <button className={`flex justify-center items-center bg-transparent rounded-full border-primary border p-3 m-0 w-14 h-14 ${className}`} onClick={onClick}>
       {children}
     </button>
   );

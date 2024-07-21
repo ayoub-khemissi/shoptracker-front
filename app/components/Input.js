477ddf16a98176eb82b63be67e515cd8702abb61
Sungis18 <khemayoub@gmail.com>
2024-07-21 19:12:07 +0200

    fix: update Input border radius (more rounded)

--- app/components/Input.js
+++ app/components/Input.js
@@ -8,7 +8,7 @@ const Input = ({ className = "", labelText = null, type, placeholder, value, onC
       {labelText &&
         <label className={`${montserrat.className} uppercase pb-1`}>{labelText}</label>
       }
-      <input className="py-2 px-4 border-2 rounded border-primary bg-contrast placeholder-gray" placeholder={placeholder} value={value} type={type} onChange={onChange} />
+      <input className="py-2 px-4 border-2 rounded-md border-primary bg-contrast placeholder-gray" placeholder={placeholder} value={value} type={type} onChange={onChange} />
     </div>
   );
 }

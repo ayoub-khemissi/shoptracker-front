eef15e4c832306058b3b8fc8094cdf477535f313
Sungis18 <khemayoub@gmail.com>
2024-07-23 00:18:03 +0200

    feat: update NavLink component accept more colors variants

--- app/components/NavLink.js
+++ app/components/NavLink.js
@@ -3,10 +3,25 @@ import Link from "next/link";
 
 const lexend = Lexend({ weight: "700", subsets: ["latin"] });
 
-const NavLink = ({ children, className = "", href }) => {
+const NavLink = ({ children, type="primary", className = "", href }) => {
+  let typeClass;
 
+  switch (type) {
+    case "primary":
+      typeClass = "text-primary hover:text-secondary";
+      break;
+
+    case "tertiary":
+      typeClass = "text-primary hover:text-tertiary";
+      break;
+
+    case "contrast":
+      typeClass = "text-contrast hover:text-tertiary";
+      break;
+  }
+  
   return (
-    <Link href={href} className={`${lexend.className} text-primary hover:text-secondary transition duration-200 uppercase text-xl ${className}`}>{children}</Link>
+    <Link href={href} className={`${lexend.className} ${typeClass} transition duration-200 uppercase text-xl ${className}`}>{children}</Link>
   );
 }
 

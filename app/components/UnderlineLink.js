396ef5e531510945b0b23905007d6eca861c8ff1
Sungis18 <khemayoub@gmail.com>
2024-07-23 02:25:33 +0200

    feat: add target property to UnderlineLink component

--- app/components/UnderlineLink.js
+++ app/components/UnderlineLink.js
@@ -1,9 +1,9 @@
 
 import Link from "next/link";
 
-const UnderlineLink = ({ children, className = "", href }) => {
+const UnderlineLink = ({ children, className = "", href, target = "_self" }) => {
   return (
-    <Link href={href} className={`text-primary underline hover:text-secondary transition duration-200 ${className}`}>{children}</Link>
+    <Link target={target} href={href} className={`text-primary underline hover:text-secondary transition duration-200 ${className}`}>{children}</Link>
   );
 }
 

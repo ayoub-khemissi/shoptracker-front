409f2b3b4007ed1c83b9e01fa43057801295f9e9
Sungis18 <khemayoub@gmail.com>
2024-07-23 03:28:13 +0200

    feat: add some utils functions - format price - trunctate string

--- utils/Helper.js
+++ utils/Helper.js
@@ -4,7 +4,7 @@ export function convertMilliseconds(ms) {
   const hour = 60 * minute;
 
   if (ms < second) {
-    return "less than a second";
+    return "in a second";
   } else if (ms < minute) {
     let seconds = Math.floor(ms / second);
     return seconds + (seconds === 1 ? " second" : " seconds");
@@ -18,8 +18,16 @@ export function convertMilliseconds(ms) {
 }
 
 export function formatMonthlyAnnuallyPrice(monthlyAnnually, price) {
-  if (!price || price == 0) { return "0"; }
+  if (!price || price == 0) { return 0; }
   const annuallyCalc = Math.round(price * 12 * 0.75) - 0.01;
 
-  return String(monthlyAnnually ? annuallyCalc : price).replace(".", ",");
+  return formatPrice(monthlyAnnually ? annuallyCalc : price);
+}
+
+export function formatPrice(price) {
+  return String(price).replace(".", ",");
+}
+
+export function truncateString(str, num) {
+  return str.length > num ? str.slice(0, num) + "..." : str;
 }

5a2c862c21e97df2dc803b7d415ac10d22dfc582
Sungis18 <khemayoub@gmail.com>
2024-08-14 02:30:12 +0200

    feat: Settings page Subscriptions tab

--- app/components/SubscriptionInfo.js
+++ app/components/SubscriptionInfo.js
@@ -5,31 +5,39 @@ import TextLabel from "./TextLabel";
 import TextSeparator from "./TextSeparator";
 
 const SubscriptionInfo = ({ className = "", planInfo, subscriptionInfo }) => {
+  if (!subscriptionInfo) {
+    return (
+      <div className={`flex w-full flex-wrap justify-center ${className}`}>
+        <Subscription callToAction={false} planInfo={planInfo} />
+      </div>
+    );
+  }
+
   const { startDate, nextPaymentDate, paymentMethod, paymentHistory } = subscriptionInfo;
 
   return (
     <div className={`flex w-full flex-wrap justify-evenly ${className}`}>
       <Subscription callToAction={false} planInfo={planInfo} />
-      <div className="w-96 space-y-5 py-4 lg:py-0">
-        <TextSeparator>Subscription</TextSeparator>
-        <div className="flex items-center justify-between">
+      <div className="w-60 space-y-5 py-4 md:w-96 lg:py-0">
+        <TextSeparator className="w-full">Subscription</TextSeparator>
+        <div className="flex w-full items-center justify-between">
           <TextLabel>Start date</TextLabel>
           <TextImportant className="text-right">
             {new Date(startDate).toLocaleString()}
           </TextImportant>
         </div>
-        <div className="flex items-center justify-between">
+        <div className="flex w-full items-center justify-between">
           <TextLabel>Next payment date</TextLabel>
           <TextImportant className="text-right">
             {new Date(nextPaymentDate).toLocaleString()}
           </TextImportant>
         </div>
-        <div className="flex items-center justify-between">
+        <div className="flex w-full items-center justify-between">
           <TextLabel>Payment Method</TextLabel>
           <TextImportant className="text-right">{paymentMethod}</TextImportant>
         </div>
-        <TextSeparator>Payment history</TextSeparator>
-        <div className="flex flex-col justify-center space-y-3">
+        <TextSeparator className="w-full">Payment history</TextSeparator>
+        <div className="flex w-full flex-col justify-center space-y-3">
           {paymentHistory.slice(0, 6).map((payment) => {
             return (
               <div
--- app/settings/page.js
+++ app/settings/page.js
@@ -7,12 +7,27 @@ import TextLabel from "../components/TextLabel";
 import Image from "next/image";
 import TextSeparator from "../components/TextSeparator";
 import Switch from "../components/Switch";
+import SubscriptionInfo from "../components/SubscriptionInfo";
+import Constants from "@/utils/Constants";
 
 export default function Settings() {
   const [tab, setTab] = useState("profile");
   const [notificationMailbox, setNotificationMailbox] = useState(true);
   const [notificationTextMessage, setNotificationTextMessage] = useState(true);
   const [notificationBrowser, setNotificationBrowser] = useState(true);
+  const [activeSubscription, setActiveSubscription] = useState({
+    planInfo: {
+      id: Constants.SUBSCRIPTION_PLAN_ID_FREE,
+      monthlyAnnually: false,
+      title: "Free Plan",
+      price: 0,
+      trackCheckInterval: 21600000,
+      trackEnabledMaxProducts: 1,
+      trackDisabledMaxProducts: 5,
+      trackMaxUserSearchesPerDay: 5,
+    },
+  });
+  const [canceledOrExpiredSubscriptions, setCanceledOrExpiredSubscriptions] = useState([]);
 
   return (
     <main className="h-full space-y-3 bg-gradient-to-b from-contrast from-90% to-contrast-alt px-10 md:px-20 lg:px-40">
@@ -42,18 +57,18 @@ export default function Settings() {
         <Button
           className="mb-2"
           locked
-          type={tab === "subscription-history" ? "primary" : "contrast"}
+          type={tab === "subscriptions" ? "primary" : "contrast"}
           defaultCursor={true}
           onClick={() => {
-            setTab("subscription-history");
+            setTab("subscriptions");
           }}
         >
-          Subscription history
+          Subscriptions
         </Button>
       </div>
       {tab === "profile" && (
-        <div className="space-y-5">
-          <div className="flex items-center justify-center space-x-4">
+        <div className="flex flex-col items-center justify-center space-y-5">
+          <div className="flex items-center justify-center">
             <Image
               className="rounded-full"
               src="https://lh3.googleusercontent.com/a/ACg8ocLYaqsM5UXK9HxKhvBQL_koiF-ceLh9FBRL513Y0zNHXo776OZI=s288-c-no"
@@ -62,77 +77,93 @@ export default function Settings() {
               alt="profile picture"
             />
           </div>
-          <TextSeparator>Personnal Information</TextSeparator>
-          <div className="flex items-center justify-between">
-            <TextLabel>Email</TextLabel>
-            <Input className="w-60 md:w-96" placeholder="xyz@mail.com" disabled type="email" />
+          <div className="flex w-full flex-wrap items-center justify-evenly">
+            <div className="flex w-60 flex-col items-center justify-center space-y-4 py-4 md:w-96">
+              <TextSeparator className="w-full">Personnal Information</TextSeparator>
+              <Input
+                labelText="Email"
+                className="w-full"
+                placeholder="xyz@mail.com"
+                disabled
+                type="email"
+              />
+              <Input labelText="Firstname" className="w-full" placeholder="Jane" type="text" />
+              <Input labelText="Lastname" className="w-full" placeholder="Doe" type="text" />
+              <Input
+                labelText="Phone number"
+                className="w-full"
+                placeholder="+000123456789"
+                type="text"
+              />
+            </div>
+            <div className="flex w-60 flex-col items-center justify-center space-y-4 py-4 md:w-96">
+              <TextSeparator className="w-full">Address Information</TextSeparator>
+              <Input labelText="Country" className="w-full" placeholder="Country" type="text" />
+              <Input labelText="City" className="w-full" placeholder="City" type="text" />
+              <Input labelText="Address" className="w-full" placeholder="Address" type="text" />
+              <Input labelText="Zipcode" className="w-full" placeholder="Zipcode" type="text" />
+            </div>
           </div>
-          <div className="flex items-center justify-between space-x-4">
-            <TextLabel>Firstname</TextLabel>
-            <Input className="w-60 md:w-96" placeholder="Jane" type="text" />
-          </div>
-          <div className="flex items-center justify-between space-x-4">
-            <TextLabel>Lastname</TextLabel>
-            <Input className="w-60 md:w-96" placeholder="Doe" type="text" />
-          </div>
-          <div className="flex items-center justify-between space-x-4">
-            <TextLabel>Phone number</TextLabel>
-            <Input className="w-60 md:w-96" placeholder="+000123456789" type="text" />
-          </div>
-          <TextSeparator>Address Information</TextSeparator>
-          <div className="flex items-center justify-between space-x-4">
-            <TextLabel>Country</TextLabel>
-            <Input className="w-60 md:w-96" placeholder="Country" type="text" />
-          </div>
-          <div className="flex items-center justify-between space-x-4">
-            <TextLabel>City</TextLabel>
-            <Input className="w-60 md:w-96" placeholder="City" type="text" />
-          </div>
-          <div className="flex items-center justify-between space-x-4">
-            <TextLabel>Address</TextLabel>
-            <Input className="w-60 md:w-96" placeholder="Address" type="text" />
-          </div>
-          <div className="flex items-center justify-between space-x-4">
-            <TextLabel>Zipcode</TextLabel>
-            <Input className="w-60 md:w-96" placeholder="Zipcode" type="text" />
-          </div>
-          <div className="flex items-center justify-end">
-            <Button onClick={() => {}}>Update</Button>
+          <div className="flex w-full items-center justify-center">
+            <Button onClick={() => {}}>Save profile</Button>
           </div>
         </div>
       )}
       {tab === "notifications" && (
-        <div className="space-y-5">
-          <TextSeparator>Receive a notification</TextSeparator>
-          <div
-            className="flex items-center justify-between space-x-4"
-            onClick={() => {
-              setNotificationTextMessage(!notificationTextMessage);
-            }}
-          >
-            <TextLabel>By text message</TextLabel>
-            <Switch checked={notificationTextMessage} />
+        <div className="flex w-full items-center justify-center py-4">
+          <div className="flex w-60 flex-col items-center justify-evenly space-y-5 md:w-96">
+            <TextSeparator className="w-full">Receive a notification</TextSeparator>
+            <div
+              className="flex w-full items-center justify-between"
+              onClick={() => {
+                setNotificationTextMessage(!notificationTextMessage);
+              }}
+            >
+              <TextLabel>By text message</TextLabel>
+              <Switch checked={notificationTextMessage} />
+            </div>
+            <div
+              className="flex w-full items-center justify-between"
+              onClick={() => {
+                setNotificationMailbox(!notificationMailbox);
+              }}
+            >
+              <TextLabel>In your mailbox</TextLabel>
+              <Switch checked={notificationMailbox} />
+            </div>
+            <div
+              className="flex w-full items-center justify-between"
+              onClick={() => {
+                setNotificationBrowser(!notificationBrowser);
+              }}
+            >
+              <TextLabel>On your browser</TextLabel>
+              <Switch checked={notificationBrowser} />
+            </div>
+            <div className="flex w-full items-center justify-center">
+              <Button onClick={() => {}}>Save notifications</Button>
+            </div>
           </div>
-          <div
-            className="flex items-center justify-between"
-            onClick={() => {
-              setNotificationMailbox(!notificationMailbox);
-            }}
-          >
-            <TextLabel>In your mailbox</TextLabel>
-            <Switch checked={notificationMailbox} />
-          </div>
-          <div
-            className="flex items-center justify-between"
-            onClick={() => {
-              setNotificationBrowser(!notificationBrowser);
-            }}
-          >
-            <TextLabel>On your browser</TextLabel>
-            <Switch checked={notificationBrowser} />
-          </div>
-          <div className="flex items-center justify-end">
-            <Button onClick={() => {}}>Update</Button>
+        </div>
+      )}
+      {tab === "subscriptions" && (
+        <div className="flex w-full items-center justify-center py-4">
+          <div className="flex w-60 flex-col items-center justify-center space-y-5 md:w-96">
+            <TextSeparator className="w-full">Active</TextSeparator>
+            <SubscriptionInfo
+              subscriptionInfo={activeSubscription.subscriptionInfo}
+              planInfo={activeSubscription.planInfo}
+            />
+            <TextSeparator className="w-full">Canceled or expired</TextSeparator>
+            {canceledOrExpiredSubscriptions.slice(0, 3).map((subscription) => {
+              return (
+                <SubscriptionInfo
+                  key={`subscription-${subscription.id}`}
+                  subscriptionInfo={subscription.subscriptionInfo}
+                  planInfo={subscription.planInfo}
+                />
+              );
+            })}
           </div>
         </div>
       )}

c3364bfc7b72a755e32c0fe92c7cdafd3eca3140
Sungis18 <khemayoub@gmail.com>
2024-08-17 21:07:43 +0200

    feat: init new pages - Checkout - Cookies - Password Recovery - Privacy - Terms of Sale - Terms of Use

---
+++ app/terms-of-sale/page.js
@@ -0,0 +1,7 @@
+"use client";
+
+export default function TermsOfSale() {
+  return (
+    <div className="h-full space-x-0 space-y-4 bg-gradient-to-b from-contrast from-90% to-contrast-alt px-6 md:px-20 lg:flex lg:space-x-20 lg:space-y-0 lg:px-40"></div>
+  );
+}

60fdc61c7b8ffe976860339d69a8133cbf6b261c
Sungis18 <khemayoub@gmail.com>
2024-08-17 20:42:54 +0200

    fix: InvisibleButton onClick default value function not object

--- app/components/InvisibleButton.js
+++ app/components/InvisibleButton.js
@@ -1,4 +1,4 @@
-const InvisibleButton = ({ children, className = "", onClick = {} }) => {
+const InvisibleButton = ({ children, className = "", onClick = () => {} }) => {
   return (
     <button className={`m-0 border-none bg-transparent p-0 ${className}`} onClick={onClick}>
       {children}

bd4e858a046f20e4ee7cf918b1b67e92089bd8be
Sungis18 <khemayoub@gmail.com>
2024-09-18 18:51:17 +0200

    chore: remove unused page

--- app/checkout/page.js
+++
@@ -1,7 +0,0 @@
-"use client";
-
-export default function Checkout() {
-  return (
-    <div className="h-full space-x-0 space-y-4 bg-gradient-to-b from-contrast from-90% to-contrast-alt px-6 md:px-20 lg:flex lg:space-x-20 lg:space-y-0 lg:px-40"></div>
-  );
-}

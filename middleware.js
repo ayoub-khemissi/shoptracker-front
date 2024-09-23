15ead6d398196560e4d9d07775cf68c236d84010
Sungis18 <khemayoub@gmail.com>
2024-09-23 23:56:47 +0200

    feat: Middleware route middleware - Redirect users to the good pages

---
+++ middleware.js
@@ -0,0 +1,33 @@
+import { NextResponse } from "next/server";
+
+export function middleware(req) {
+  const jwt = req.cookies.get("jwt");
+  const url = req.nextUrl.pathname;
+
+  if (
+    jwt &&
+    (url.startsWith("/login") || url.startsWith("/register") || url.startsWith("/account-recovery"))
+  ) {
+    return NextResponse.redirect(new URL("/tracklist", req.url));
+  }
+
+  if (
+    !jwt &&
+    (url.startsWith("/settings") || url.startsWith("/tracker") || url.startsWith("/tracklist"))
+  ) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    "/login/:path*",
+    "/register/:path*",
+    "/account-recovery/:path*",
+    "/settings/:path*",
+    "/tracker/:path*",
+    "/tracklist/:path*",
+  ],
+};

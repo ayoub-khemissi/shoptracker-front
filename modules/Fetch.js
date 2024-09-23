e13ab9026cb5354afa8d43c1fb1f0af4046a6c15
Sungis18 <khemayoub@gmail.com>
2024-09-23 23:58:47 +0200

    feat: Fetch module to send http requests to the API

---
+++ modules/Fetch.js
@@ -0,0 +1,34 @@
+import {
+  NEXT_PUBLIC_SHOPTRACKER_API_PORT,
+  NEXT_PUBLIC_SHOPTRACKER_API_HOSTNAME,
+  NEXT_PUBLIC_SHOPTRACKER_API_HTTPSECURE,
+} from "../utils/Config";
+
+const apiUrl = `http${NEXT_PUBLIC_SHOPTRACKER_API_HTTPSECURE ? "s" : ""}://${NEXT_PUBLIC_SHOPTRACKER_API_HOSTNAME}${NEXT_PUBLIC_SHOPTRACKER_API_HTTPSECURE ? "" : `:${NEXT_PUBLIC_SHOPTRACKER_API_PORT}`}`;
+
+export const fetchData = async (path, body, method = "GET", authentified = false) => {
+  try {
+    const response = await fetch(apiUrl + path, {
+      method: method,
+      body: JSON.stringify(body),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: authentified ? "include" : "omit",
+    });
+
+    if (response.status === 401) {
+      await fetch(apiUrl + "/logout", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+      });
+    }
+
+    return response;
+  } catch (error) {
+    console.error("Fetch error:", error);
+  }
+};

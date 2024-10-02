93cc12a5a58084f2461b49c67ad1da42b46f92ef
Sungis18 <khemayoub@gmail.com>
2024-10-03 01:44:07 +0200

    feat: Input component add show/hide password mechanic

--- app/components/Input.js
+++ app/components/Input.js
@@ -1,4 +1,10 @@
+import { useState } from "react";
 import { Montserrat } from "next/font/google";
+import TextNormal from "./TextNormal";
+import EyeShowSvg from "../../public/assets/svg/icons/eye-show.svg";
+import EyeHideSvg from "../../public/assets/svg/icons/eye-hide.svg";
+import Image from "next/image";
+import InvisibleButton from "./InvisibleButton";
 
 const montserrat = Montserrat({ weight: "500", subsets: ["latin"] });
 
@@ -12,35 +18,56 @@ const Input = ({
   step,
   min,
   max,
-  disabled = false,
-  required = false,
+  disabled,
+  required,
+  errorText = null,
+  isError = false,
   onChange = () => {},
 }) => {
-  const classNames = `py-2 px-4 border-2 rounded-md border-primary bg-contrast placeholder-gray ${disabled ? "opacity-50 cursor-not-allowed" : ""} ${className}`;
+  const [showPassword, setShowPassword] = useState(false);
+  const isPassword = type === "password";
+  const classNames = `py-2 px-4 border-2 rounded-md ${
+    isError ? "border-error text-error" : "border-primary text-primary"
+  } bg-contrast placeholder-gray ${disabled ? "opacity-50 cursor-not-allowed" : ""} ${className}`;
 
   return (
-    <div className={`flex flex-col ${className}`}>
+    <div className={`flex flex-col space-y-2 ${className}`}>
       {labelText && (
-        <label id={`label-${id}`} htmlFor={id} className={`${montserrat.className} pb-1 uppercase`}>
+        <label id={`label-${id}`} htmlFor={id} className={`${montserrat.className} uppercase`}>
           {labelText}
         </label>
       )}
-      <input
-        id={id}
-        name={id}
-        className={classNames}
-        placeholder={placeholder}
-        value={value}
-        type={type}
-        step={step}
-        onChange={onChange}
-        disabled={disabled}
-        required={required}
-        aria-required={required}
-        {...(min && { min: min })}
-        {...(max && { max: max })}
-        {...(labelText && { "aria-labelledby": `label-${id}` })}
-      />
+      <div className="relative">
+        <input
+          id={id}
+          name={id}
+          className={classNames}
+          placeholder={placeholder}
+          value={value}
+          type={isPassword && showPassword ? "text" : type}
+          onChange={onChange}
+          {...(step && { step: step })}
+          {...(disabled && { disabled: disabled })}
+          {...(required && { required: required, "aria-required": required })}
+          {...(min && { min: min })}
+          {...(max && { max: max })}
+          {...(labelText && { "aria-labelledby": `label-${id}` })}
+        />
+        {isPassword && (
+          <InvisibleButton
+            onClick={() => setShowPassword(!showPassword)}
+            className="absolute right-4 top-1/2 -translate-y-1/2 transform"
+          >
+            <Image
+              className="h-6 w-6"
+              title={showPassword ? "Hide password" : "Show password"}
+              src={showPassword ? EyeHideSvg : EyeShowSvg}
+              alt="show hide password"
+            />
+          </InvisibleButton>
+        )}
+      </div>
+      {isError && <TextNormal className="text-sm text-error">{errorText}</TextNormal>}
     </div>
   );
 };

f9a32c1c3bc6704e92ee93d86412ef9f0e841eda
Sungis18 <khemayoub@gmail.com>
2024-10-04 18:16:51 +0200

    feat: Header component add toast when logout

--- app/components/Header.js
+++ app/components/Header.js
@@ -58,6 +58,7 @@ const Header = () => {
                       return;
                     }
 
+                    showToast("See you later 👋", "info");
                     await localLogout();
                   }}
                 >

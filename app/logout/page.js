f856d31d55c32b12a5e9c8e828b1f9ad22b9ff7c
Sungis18 <khemayoub@gmail.com>
2024-10-22 06:57:53 +0200

    feat: Logout rework fetch

--- app/logout/page.js
+++ app/logout/page.js
@@ -16,14 +16,17 @@ export default function Logout() {
     const logout = async () => {
       const response = await fetchLogout();
 
-      if (!response || response.status !== 200) {
-        showToast("Failed to logout. Please try again later.", "error");
-        router.push("/");
-        return;
-      }
+      switch (response?.status) {
+        case 200:
+          showToast("See you later 👋", "info");
+          await localLogout();
+          break;
 
-      showToast("See you later 👋", "info");
-      await localLogout();
+        default:
+          showToast("Failed to logout. Please try again later.", "error");
+          router.push("/");
+          break;
+      }
     };
 
     logout();
@@ -32,7 +35,7 @@ export default function Logout() {
   return (
     <div className="h-full space-x-0 space-y-4 bg-gradient-to-b from-contrast from-90% to-contrast-alt px-6 md:px-20 lg:flex lg:space-x-20 lg:space-y-0 lg:px-40">
       <div className="flex w-full items-center justify-center">
-        <Spinner />
+        <Spinner className="h-12 w-12" />
       </div>
     </div>
   );

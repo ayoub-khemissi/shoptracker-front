97d6118f038944f5b34a71927933e57b218c1fb8
Sungis18 <khemayoub@gmail.com>
2024-10-27 16:44:06 +0100

    feat: Modal display adjustments

--- app/components/Modal.js
+++ app/components/Modal.js
@@ -27,7 +27,7 @@ const Modal = ({ children, isVisible = false, onClose = () => {} }) => {
   if (!isVisible) return null;
 
   return (
-    <div className="fixed inset-0 z-10">
+    <div className="fixed inset-0 z-10 w-full">
       <div className="flex min-h-screen items-center justify-center p-4 text-center sm:block sm:p-0">
         <div className="fixed inset-0 transition-opacity" aria-hidden="true" onClick={onClose}>
           <div className="absolute inset-0 bg-black opacity-50"></div>
@@ -36,7 +36,7 @@ const Modal = ({ children, isVisible = false, onClose = () => {} }) => {
         <span className="sm:inline-block sm:h-screen sm:align-middle">&#8203;</span>
 
         <div
-          className="inline-block transform overflow-hidden rounded-lg bg-contrast text-left align-bottom shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:align-middle"
+          className="inline-block w-full transform overflow-hidden rounded-lg bg-contrast text-left align-bottom shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:align-middle"
           role="dialog"
           aria-modal="true"
           aria-labelledby="modal-headline"

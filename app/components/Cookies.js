deeeae476159c9b7ac08fd9d99da20461ac644e1
Sungis18 <khemayoub@gmail.com>
2024-12-31 15:54:49 +0100

    feat: Cookies component completing info and modal display management

--- app/components/Cookies.js
+++ app/components/Cookies.js
@@ -5,9 +5,12 @@ import Modal from "./Modal";
 import Title from "./Title";
 import Button from "./Button";
 import TextNormal from "./TextNormal";
+import UnderlineLink from "./UnderlineLink";
+import { usePathname } from "next/navigation";
 
 const Cookies = () => {
   const [cookieModalVisible, setCookieModalVisible] = useState(false);
+  const currentPathname = usePathname();
 
   useEffect(() => {
     const consent = localStorage.getItem("cookieConsent");
@@ -19,6 +22,10 @@ const Cookies = () => {
     setCookieModalVisible(false);
   };
 
+  if (currentPathname === "/privacy") {
+    return null;
+  }
+
   return (
     <Modal
       isVisible={cookieModalVisible}
@@ -27,25 +34,35 @@ const Cookies = () => {
       }}
       isClosable={false}
     >
-      <Title className="pb-4 text-center text-2xl">
-        We Only Use
-        <br />
-        <span className="text-secondary">Essential Cookies</span>!
-      </Title>
-      <TextNormal className="pb-4">
-        At ShopTracker, your privacy is our priority. We only use cookies necessary for secure
-        login, navigation, and maintaining your preferences.
-        <br />
-        <br />
-        Rest assured, we don't use tracking or advertising cookies.
-        <br />
-        <br />
-        Thank you for trusting ShopTracker!
-      </TextNormal>
-      <div className="flex items-center justify-center">
-        <Button type="primary" onClick={handleAcceptCookies} className="self-end">
-          Let's track!
-        </Button>
+      <div className="flex flex-col items-center justify-center text-center">
+        <Title className="text-center text-2xl">
+          We Only Use
+          <br />
+          <span className="text-secondary">Essential Cookies</span>!
+        </Title>
+        <TextNormal className="py-4">
+          At ShopTracker, your privacy is our priority. We only use cookies necessary for secure
+          login, navigation, and maintaining your preferences.
+          <br />
+          <br />
+          Rest assured, we don't use tracking or advertising cookies.
+          <br />
+          <br />
+          By continuing to use ShopTracker, you consent to the use of these essential cookies and
+          agree to our <br />
+          <UnderlineLink href="/privacy" className="text-secondary hover:text-tertiary">
+            Privacy Policy
+          </UnderlineLink>
+          .
+          <br />
+          <br />
+          Thank you for trusting ShopTracker!
+        </TextNormal>
+        <div className="flex items-center justify-center">
+          <Button type="primary" onClick={handleAcceptCookies} className="self-end">
+            Let's track!
+          </Button>
+        </div>
       </div>
     </Modal>
   );

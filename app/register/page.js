c8bb8c43641057b2d97dac2403030b0255f7c79c
Sungis18 <khemayoub@gmail.com>
2025-04-06 06:44:16 +0200

    fix: Login & Register pages responsive

--- app/register/page.js
+++ app/register/page.js
@@ -18,6 +18,7 @@ import { signIn, signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { validatePassword } from "@/modules/DataValidation";
 import { Checkbox } from "@nextui-org/react";
+import { Section } from "../components/Section";
 
 export default function Register() {
   const [email, setEmail] = useState("");
@@ -139,95 +140,94 @@ export default function Register() {
         name="description"
         content="Sign up page for ShopTracker. This page allows users to create an account and sign up for the service."
       />
-      <div className="h-full space-x-0 space-y-4 bg-gradient-to-b from-contrast from-90% to-contrast-alt px-6 md:px-20 lg:flex lg:space-x-20 lg:space-y-0 lg:px-40">
-        <section className="hidden justify-center space-y-4 text-center lg:flex lg:w-1/2 lg:flex-col lg:text-left">
-          <ShopTrackerLogo className="text-5xl" />
-          <TextNormal className="text-3xl">
-            Get an alert on the availability and{" "}
-            <span className="text-secondary transition duration-200 hover:text-tertiary">
-              price drop of your favorite products
-            </span>
-            !
-          </TextNormal>
-        </section>
-        <section className="flex flex-col items-center space-y-4 lg:w-1/2">
-          <Title className="text-center text-2xl lg:text-4xl">Sign Up</Title>
-          <CircleButton
-            onClick={() => {
-              if (!isTermsAccepted) {
-                showToast("Please accept the terms and conditions.", "error");
-                return;
-              }
+      <Section>
+        <div className="flex items-center justify-center">
+          <div className="hidden justify-center space-y-4 text-center lg:flex lg:w-1/2 lg:flex-col lg:text-left">
+            <ShopTrackerLogo className="text-5xl" />
+            <TextNormal className="text-3xl">
+              Get an alert on the availability and{" "}
+              <span className="text-secondary transition duration-200 hover:text-tertiary">
+                price drop of your favorite products
+              </span>
+              !
+            </TextNormal>
+          </div>
+          <div className="flex w-full flex-col items-center space-y-4 lg:w-1/2">
+            <Title className="text-center text-2xl lg:text-4xl">Sign Up</Title>
+            <CircleButton
+              onClick={() => {
+                if (!isTermsAccepted) {
+                  showToast("Please accept the terms and conditions.", "error");
+                  return;
+                }
 
-              signIn("google", {
-                callbackUrl: "/register",
-              });
-            }}
-          >
-            <Image className="h-6 w-6" src={GoogleLogoSvg} alt="google sign" />
-          </CircleButton>
-          <TextSeparator className="w-full">Or</TextSeparator>
-          <form className="w-full space-y-4" onSubmit={registerClassical}>
-            <Input
-              id="email"
-              className="w-full"
-              labelText="Email"
-              type="email"
-              placeholder="xyz@mail.com"
-              value={email}
-              required
-              isError={isErrorEmail}
-              onChange={(e) => {
-                setEmail(e.target.value);
-                setIsErrorEmail(false);
+                signIn("google", {
+                  callbackUrl: "/register",
+                });
               }}
-            />
-            <Input
-              id="password"
-              className="w-full"
-              labelText="Password"
-              type="password"
-              placeholder="••••••••••••"
-              value={password}
-              required
-              isError={isErrorPassword}
-              pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$"
-              errorText="The password must be at least 8 characters long and contain at least one lowercase letter, one uppercase letter, one digit, and one special character."
-              onChange={(e) => {
-                setPassword(e.target.value);
-                setIsErrorPassword(!validatePassword(e.target.value));
-              }}
-            />
-            <div className="flex w-full items-start justify-between">
-              <div className="flex w-full flex-col space-y-4">
-                <Checkbox
-                  defaultSelected
-                  color="warning"
-                  isRequired
-                  required
-                  aria-required
-                  isSelected={isTermsAccepted}
-                  onChange={(e) => setIsTermsAccepted(e.target.checked)}
-                >
-                  By signing up, you agree to our{" "}
-                  <UnderlineLink href="/terms-of-service">Terms of Service</UnderlineLink>,{" "}
-                  <UnderlineLink href="/terms-of-sale">Terms of Sale</UnderlineLink> and{" "}
-                  <UnderlineLink href="/privacy">GDPR Privacy Policy</UnderlineLink>.
-                </Checkbox>
-                <TextNormal>
-                  Already have an account? <UnderlineLink href="/login">Sign In</UnderlineLink>
-                </TextNormal>
-                <div>
-                  <UnderlineLink href="/account-recovery">Forgot password?</UnderlineLink>
+            >
+              <Image className="h-6 w-6" src={GoogleLogoSvg} alt="google sign" />
+            </CircleButton>
+            <TextSeparator className="w-full">Or</TextSeparator>
+            <form className="w-full space-y-4" onSubmit={registerClassical}>
+              <Input
+                id="email"
+                className="w-full"
+                labelText="Email"
+                type="email"
+                placeholder="xyz@mail.com"
+                value={email}
+                required
+                isError={isErrorEmail}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                  setIsErrorEmail(false);
+                }}
+              />
+              <Input
+                id="password"
+                className="w-full"
+                labelText="Password"
+                type="password"
+                placeholder="••••••••••••"
+                value={password}
+                required
+                isError={isErrorPassword}
+                pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$"
+                errorText="The password must be at least 8 characters long and contain at least one lowercase letter, one uppercase letter, one digit, and one special character."
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                  setIsErrorPassword(!validatePassword(e.target.value));
+                }}
+              />
+              <div className="flex w-full items-start justify-between">
+                <div className="flex w-full flex-col space-y-4">
+                  <Checkbox
+                    defaultSelected
+                    color="warning"
+                    isRequired
+                    required
+                    aria-required
+                    isSelected={isTermsAccepted}
+                    onChange={(e) => setIsTermsAccepted(e.target.checked)}
+                  >
+                    By signing up, you agree to our{" "}
+                    <UnderlineLink href="/terms-of-service">Terms of Service</UnderlineLink>,{" "}
+                    <UnderlineLink href="/terms-of-sale">Terms of Sale</UnderlineLink> and{" "}
+                    <UnderlineLink href="/privacy">GDPR Privacy Policy</UnderlineLink>.
+                  </Checkbox>
+                  <TextNormal>
+                    Already have an account? <UnderlineLink href="/login">Sign In</UnderlineLink>
+                  </TextNormal>
                 </div>
+                <Button buttonType="submit" type="primary">
+                  Sign Up
+                </Button>
               </div>
-              <Button buttonType="submit" type="primary">
-                Sign Up
-              </Button>
-            </div>
-          </form>
-        </section>
-      </div>
+            </form>
+          </div>
+        </div>
+      </Section>
     </>
   );
 }

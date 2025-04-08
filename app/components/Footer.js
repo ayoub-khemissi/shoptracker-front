3e0ede08b210a1dc5e0497aee6ce54341a0dee45
Sungis18 <khemayoub@gmail.com>
2025-04-08 22:26:32 +0200

    feat: Footer component beautify

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -5,7 +5,7 @@ import ShopTrackerLogo from "./ShopTrackerLogo";
 
 const Footer = () => {
   return (
-    <footer className="flex h-14 flex-wrap items-center justify-center gap-x-4 bg-contrast-alt px-6 py-2 md:px-12 lg:-mt-14 lg:px-28">
+    <footer className="z-10 flex h-14 flex-wrap items-center justify-center gap-x-4 from-purple-500/15 via-transparent to-contrast/80 px-6 py-2 md:px-12 lg:-mt-14 lg:px-28">
       <p className="text-sm">
         © 2024 <ShopTrackerLogo className="text-sm" />
       </p>
